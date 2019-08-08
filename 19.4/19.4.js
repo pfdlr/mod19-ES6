09b9ad66c2bf85728cf8a17d40f0f51219b3e80f
Piotr Fidler <fidler.piotr@gmail.com>
2019-08-08 10:22:14 +0200

    delete redundand declaration

--- 19.4/19.4.js
+++ 19.4/19.4.js
@@ -13,19 +13,19 @@ const average = (...args) => {
 	args.forEach(number => (result += number));
 	return result / args.length;
 };
-console.log(average(6, 2, 3, 4, 5, 6));//4.333333333333333
+console.log(average(6, 2, 3, 4, 5, 6)); //4.333333333333333
 
 //task 4
 const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
 const [...args] = grades;
-const average = (...args) => {
+average = (...args) => {
 	let result = 0;
 	args.forEach(number => (result += number));
-	return result  / args.length;
+	return result / args.length;
 };
-console.log(average(...args));//3.2222222222222223
+console.log(average(...args)); //3.2222222222222223
 
 //task 5
 const tbl = [1, 4, "Iwona", false, "Nowak"];
 const names = ([, , firstname, , lastname] = tbl);
-console.log(`${firstname} ${lastname}`);// Iwona Nowak
+console.log(`${firstname} ${lastname}`); // Iwona Nowak
