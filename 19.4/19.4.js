e3364353a01fe5d7ca6f800435a3ffc0af238e74
Piotr Fidler <fidler.piotr@gmail.com>
2019-08-08 11:27:03 +0200

    delete double function

--- 19.4/19.4.js
+++ 19.4/19.4.js
@@ -18,11 +18,6 @@ console.log(average(6, 2, 3, 4, 5, 6)); //4.333333333333333
 //task 4
 const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
 const [...args] = grades;
-(...args) => {
-	let result = 0;
-	args.forEach(number => (result += number));
-	return result / args.length;
-};
 console.log(average(...args)); //3.2222222222222223
 
 //task 5
